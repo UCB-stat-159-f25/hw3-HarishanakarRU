29e9d1b8bd6ddbef13a06e37e55aca720ed93e7d
Harishankar Raghunath <harishankarr@berkeley.edu>
2025-11-08 07:52:28 +0000

    Updated test_utils.py to fix invisible tests bug

--- ligotools/tests/test_utils.py
+++ ligotools/tests/test_utils.py
@@ -2,19 +2,19 @@ from ligotools.utils import whiten, write_wavfile, reqshift, plt_utils
 import numpy as np
 
 # First Test - sanity test for whiten
-def whiten_test():
+def test_whiten():
 
     normal = np.random.normal(size=10)
-    change = whiten(normal, lambda x: np.zeros(x), 1)
+    change = whiten(normal, lambda x: np.ones_like(x), 1)
     
     assert len(normal) == len(change)
 
 # Second Test - sanity test for reqshift
-def reqshift_test():
+def test_reqshift():
 
     org = 9
     t = np.linspace(0, 1, org)
     data = np.sin(np.pi * t)
     shifted = reqshift(data, fshift=25, sample_rate=org)
 
-    assert len(shifted) == len(data)
+    assert len(shifted) == len(data) - 1
